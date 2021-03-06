7653409cf6c599c3770d2bc30d725e2d3bc25884
a_frear <amy.frear@gmail.com>
2021-03-06 09:55:54 -0500

    Update tags-router.js

--- src/tags/tags-router.js
+++ src/tags/tags-router.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const TagsService = require("./TagsService");
+const TagsService = require("./tagsService");
 const tagsRouter = express.Router();
 const jsonParser = express.json();
 
