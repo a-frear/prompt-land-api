8b5bf0e916176fa292281e481e9c90d3dedc4609
a_frear <amy.frear@gmail.com>
2021-03-17 10:07:09 -0400

    Update server.js

--- src/server.js
+++ src/server.js
@@ -9,6 +9,10 @@ const db = knex({
 
 app.set("db", db);
 
+app.get("/api/*", (req, res) => {
+  res.json({ ok: true });
+});
+
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`);
 });
