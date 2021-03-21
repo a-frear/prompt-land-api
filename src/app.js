3b8d2961f48d3b0e5b5454a084ac95a3ad88cb29
a_frear <amy.frear@gmail.com>
2021-03-21 17:47:56 -0400

    Update app.js

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
 const helmet = require("helmet");
-const { NODE_ENV, CLIENT_ORIGIN } = require("./config");
+const { NODE_ENV } = require("./config");
 const promptsRouter = require("./prompts/prompts-router");
 const tagsRouter = require("./tags/tags-router");
 const usersRouter = require("./users/users-router");
@@ -18,7 +18,7 @@ app.use(morgan(morganOption));
 app.use(helmet());
 app.use(
   cors({
-      origin: CLIENT_ORIGIN
+      origin: "http://localhost:3000"
   })
 );
 
