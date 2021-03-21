0964fa97dd8c3fce1af78a79fadeda414d4ccdca
a_frear <amy.frear@gmail.com>
2021-03-21 13:20:15 -0400

    Update app.js

--- src/app.js
+++ src/app.js
@@ -18,7 +18,7 @@ app.use(morgan(morganOption));
 app.use(helmet());
 app.use(
   cors({
-      origin: 'https://prompt-land-a-frear.vercel.app/'
+      origin: 'https://prompt-land-a-frear.vercel.app'
   })
 );
 
